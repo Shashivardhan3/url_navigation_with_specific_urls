ed2ab438f2402fb159d759bc0576abbcebc6b4c3
Shashivardhan3 <107747614+Shashivardhan3@users.noreply.github.com>
2023-08-01 11:38:25 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def shashi(request):
+    return render (request,'shashi.html')
---
+++ app2/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def vardhan(request):
+    return render(request,'vardhan.html')
